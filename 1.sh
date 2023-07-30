f4c3dd6e946cae0d2b63d5bfe06f9f4e30f4b60c
ssq <2038035593@qq.com>
2023-07-30 15:55:31 +0800

    feat:增加编译NXP原版uboot的功能

--- 1.sh
+++ 1.sh
@@ -26,12 +26,26 @@ ERR="${RED}ERROR  ${CLS}"
 
 TARGET=u-boot
 TARGET_FILE=${TARGET}.bin
+TARGET_IMX_FILE=${TARGET}.imx
+IMXDOWNLOAD_TOOL=${SCRIPT_PATH}/tools/imxdownload/imxdownload
+
 SD_NODE=/dev/sdc
 
 ARCH_NAME=arm
 CROSS_COMPILE_NAME=arm-linux-gnueabihf-
+
 BOARD_CONFIG_NAME=mx6ull_14x14_evk_emmc_defconfig
 
+function time_count_down
+{
+    for i in {3..1}
+    do     
+
+        echo -ne "${INFO}after ${i} is end!!"
+        echo -ne "\r\r"        # echo -e 处理特殊字符  \r 光标移至行首，但不换行
+        sleep 1
+    done
+}
 function clean_project()
 {
     make ARCH=${ARCH_NAME} CROSS_COMPILE=${CROSS_COMPILE_NAME} distclean
@@ -68,6 +82,23 @@ function download_imx()
         echo -e "${ERR}${SD_NODE}不存在,请检查SD卡是否插入..."
         return
     fi
+    # 检查imx文件是否存在
+    if [ ! -f "${TARGET_IMX_FILE}" ];then
+        echo -e "${ERR}${TARGET_IMX_FILE} 不存在,请检查后再下载..."
+        return
+    fi
+    echo -e "${INFO}3s后开始下载 ${TARGET_IMX_FILE} 到 ${SD_NODE}..."
+    time_count_down
+    sudo dd if=${TARGET_IMX_FILE} of=${SD_NODE} bs=1k seek=1 conv=fsync
+}
+
+function download_bin()
+{
+    # 判断编译目标文件是否存在
+    if [ ! -e "${SD_NODE}" ];then
+        echo -e "${ERR}${SD_NODE}不存在,请检查SD卡是否插入..."
+        return
+    fi
     # 检查bin文件是否存在
     if [ ! -f "${TARGET_FILE}" ];then
         echo -e "${ERR}${TARGET_FILE} 不存在,请检查后再下载..."
@@ -75,10 +106,18 @@ function download_imx()
     fi
 
     ${IMXDOWNLOAD_TOOL} ${TARGET_FILE} ${SD_NODE}
+
 }
 
 function build_download_project()
 {
+    build_project
+    download_imx
+}
+
+function build_NXP_uboot()
+{
+    BOARD_CONFIG_NAME=mx6ull_14x14_evk_emmc_defconfig
     build_project
     download_imx
 }
@@ -97,6 +136,7 @@ function echo_menu()
     echo ""
     echo -e "* [0] 编译uboot工程"
     echo -e "* [1] 清理uboot工程"
+    echo -e "* [2] 编译NXP官方原版uboot工程"
     echo "================================================="
 }
 
@@ -106,6 +146,7 @@ function func_process()
 	case "${choose}" in
 		"0") build_project;;
 		"1") clean_project;;
+		"2") build_NXP_uboot;;
 		*) build_project;;
 	esac
 }
